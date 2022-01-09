d28c4f88d4f02fc40e757c8ab3fe055cd3b56357
natalijapavlovic17321 <nnatanna@elfak.rs>
2022-01-09 21:10:04 +0100

    o+z

--- Aplikacija/E-Student-Front/eStudent/assets/js/profesor.js
+++ Aplikacija/E-Student-Front/eStudent/assets/js/profesor.js
@@ -1,4 +1,5 @@
 import {predmet} from "./predmet.js";
+import {zabrana} from "./zabrana.js";
 export class profesor{
     constructor(email,ime,prezime,godina,brtel, kancelarija){
         this.kontejnerProfesor=null;
@@ -8,6 +9,7 @@ export class profesor{
         this.brtel = brtel;
         this.kancelarija = kancelarija;
         this.ispiti=[]
+        this.zabrane=[]
     }
     dodajProfesor(email,ime,prezime,godina,brtel, kancelarija){
         this.ime = ime;
@@ -19,6 +21,10 @@ export class profesor{
     dodajPredmet(p)
     {
         this.ispiti.push(p);
+    }
+    dodajZabranu(p)
+    {
+        this.zabrane.push(p);
     }
     setEmail(e)
     {
@@ -181,6 +187,141 @@ export class profesor{
         this.dodajZabranaFetch();
       });
     }
+    crtajZabranu()
+    {
+      var d5=document.getElementById("divGdeSeRadiSve");
+      d5.className="red";
+      var lab= document.createElement("h3");
+      lab.innerText="Zabrane:";
+      d5.appendChild(lab);
+      var di=document.createElement("div");
+      di.className="horizon";
+      d5.appendChild(di);
+      var l2=document.createElement("label");
+      l2.innerHTML="Predmeti:";
+      di.appendChild(l2);
+      var sel=document.createElement("select");
+      sel.style="width:200px";
+      sel.id="selectID";
+      var o=document.createElement("option");
+      o.value="";
+      o.innerHTML="";
+      o.selected=false;
+      o.style="width:200px";
+      sel.appendChild(o);
+      di.appendChild(sel);
+      this.zabrane.forEach(i=>{
+          var o=document.createElement("option");
+          o.value=i.sifra_predmeta;
+          o.innerHTML=i.sifra_predmeta;
+          o.selected=false;
+          o.style="width:200px";
+          sel.appendChild(o);
+      });
+      var l1=document.createElement("label");
+      l1.innerHTML="Email studenta:";
+      di.appendChild(l1);
+      var inp1=document.createElement("input");
+      inp1.id="emailZabrane";
+      inp1.style="width:200px";
+      di.appendChild(inp1);
+      var div2=document.createElement("div");
+      div2.style="display:flex;justify-content: center";
+      d5.appendChild(div2);
+      var baton=document.createElement("button");
+      baton.classList="btn btna";
+      baton.style="width:130px ";
+      baton.innerHTML="Pretrazi";
+      div2.appendChild(baton);
+      var divZaCrtanje=document.createElement("div");
+      d5.appendChild(divZaCrtanje);
+      this.crtajTabluZabrana(divZaCrtanje,true);
+      baton.addEventListener("click", (e) => {
+        divZaCrtanje.innerHTML="";
+        this.crtajTabluZabrana(divZaCrtanje,false);
+      });
+    }
+    crtajTabluZabrana(host,t){
+      var predmet=document.getElementById("selectID").value;
+      var student=document.getElementById("emailZabrane").innerHTML;
+      var lista=[];
+      if(t==true)
+        lista=this.zabrane;
+      else lista=this.filterZabrane(predmet,student); 
+      var table1 = document.createElement("table");
+      table1.id = "zabraneProf";
+      table1.className="zabraneProf"
+      var row1 = table1.insertRow();
+      let staticInfo = [
+        " ",
+        "Sifra predmeta",
+        "Email studenta",
+        "Razlog",
+        "Datum isteka"
+      ];
+      row1 = table1.insertRow();
+      for (let i = 0; i < staticInfo.length; i++) {
+        let cell = row1.insertCell();
+        cell.innerHTML = staticInfo[i];
+      }
+      var j=0;
+      lista.forEach(i=>{
+        row1 = table1.insertRow();
+        row1.id = i.id;
+        var cell1;
+        cell1 = row1.insertCell();
+        cell1.innerHTML = j+1;
+
+        cell1 = row1.insertCell();
+        cell1.innerHTML = i.sifra_predmeta;
+
+        cell1 = row1.insertCell();
+        cell1.innerHTML = i.email_student;
+
+        cell1 = row1.insertCell();
+        cell1.innerHTML = i.razlog;
+
+        cell1 = row1.insertCell();
+        cell1.innerHTML = i.datum_isteka.slice(0,10);
+
+        /*cell1 = row1.insertCell();
+        var dugme=document.createElement("button");
+        dugme.innerHTML="Obrisi";
+        dugme.id=i.id;
+        cell1.appendChild(dugme);
+        //cell1.innerHTML = i.smer;
+        dugme.addEventListener("click", (e) => {
+          this.obrisiJednuZabranu(i.id);   ///ovo fali
+        });*/
+        j++;
+      });
+      host.appendChild(table1);
+    }
+    filterZabrane(predemet, student)
+    {
+      var lista=[];
+      if(predemet!= null && student==null)
+         this.zabrane.forEach(z=>{
+            if(z.sifra_predmeta==predemet)
+                lista.push(z);
+         });
+      else{
+        if(student!= null && predemet!=null)
+         this.zabrane.forEach(z=>{
+            if(z.email_student==student || z.sifra_predmeta==predemet)
+                  lista.push(z);
+         });
+        else{ 
+          this.zabrane.forEach(z=>{
+             if(z.email_student==email)
+                 lista.push(z);
+          });
+        }
+      }
+      if(predemet=="" && student=="")
+         lista=this.zabrane;
+      return lista;
+    }
     crtajDodavanjeMesta()
     {
       var d5=document.getElementById("divGdeSeRadiSve");
@@ -238,7 +379,6 @@ export class profesor{
       today = yyyy + '-' + mm + '-' + dd;
       var tekst=document.getElementById("tekstObavestenja").value;
       var selectoption=document.getElementById("selectID").value;
-      console.log(today);
       if(tekst!="")
       {
       fetch("https://localhost:7078/Natalija/postObavestenje", {
@@ -344,6 +484,26 @@ export class profesor{
         });
       }
       else alert("Unesite sva polja");
-
+    }
+    prikaziZabrana(){
+      this.zabrane.length=0;
+      fetch("https://localhost:7078/Natalija/getZabrana/"+this.email, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          }
+        })
+          .then((response) => response.json())
+          .then((data) => {
+            if (data.title == "Unauthorized") alert("Lose korisnicko ime ili sifra.");
+            else {
+              data.forEach(element => {
+                  var p1 = new zabrana(element.id, element.datum_isteka,element.email_student,element.razlog,element.sifra_predmeta);
+                  this.dodajZabranu(p1);
+              });
+              this.crtajZabranu();
+            }
+          })
+          .catch((error) => console.error("Greska sa prijavljivanjem", error));
     }
 }
